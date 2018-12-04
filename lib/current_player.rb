e8191ff566f1958ae9ce5369e3f2131b52a38b6b
Jordan Ginor <jordan.ginor@gmail.com>
2018-12-04 03:51:08 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,28 @@
+
+def turn_count(array)
+  counter = 0 
+  while counter<9 
+  array.each do|index|
+    if index == "X" || index == "O"
+       counter += 1 
+    else
+      counter = 0
+    end
+end
+end
+end
+
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+turn_count(board)
+
+
+
+
+def current_player
+end
+  
+#primary_colors = ["Red", "Yellow", "Blue"]
+#primary_colors.each do |color| 
+#  puts "Primary Color #{color} is #{color.length} letters long."
+#end 
